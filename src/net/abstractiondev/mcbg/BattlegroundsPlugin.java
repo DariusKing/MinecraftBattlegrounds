e161113ed4d1694dbee31eab91bd2ce9b74cade3
Alexandra <alex@abstractiondev.net>
2017-04-20 13:48:26 -0400

    0.1.1 Initial

--- src/net/abstractiondev/mcbg/BattlegroundsPlugin.java
+++ src/net/abstractiondev/mcbg/BattlegroundsPlugin.java
@@ -1,10 +1,27 @@
 package net.abstractiondev.mcbg;
 
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
 /*
  * Battlegrounds Plugin  
  */
 
-public class BattlegroundsPlugin
+public class BattlegroundsPlugin extends JavaPlugin
 {
-
+	private Logger log;
+	
+	@Override
+	public void onEnable()
+	{
+		log = Logger.getLogger("Minecraft");
+		
+		log.info("Initializing Minecraft Battlegrounds Plugin by JuicyKitten and GrumpyBear.");
+	}
+	@Override
+	public void onDisable()
+	{
+		log.info("Disabling Minecraft Battlegrounds Plugin by JuicyKitten and GrumpyBear.");
+	}
 }
